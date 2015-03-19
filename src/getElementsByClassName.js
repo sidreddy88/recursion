bf3ad340f26037675c5bc7fd0723c1dfd592e704
Siddharth Sukumar <siddharth@inauth.com>
2015-03-19 12:01:35 -0700

    Pre course work - Finished the recursionassignment

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,29 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-){
-  // your code here
+var getElementsByClassName = function(className){ 
+
+  var result = [];
+
+  function recurse (node) {
+       
+        if(node.classList && node.classList.contains (className)){
+          if(result.indexOf(node)===-1) {
+            result.push(node);
+         }
+        }
+
+        for (var i = 0; i < node.childNodes.length; i++) {
+            if (node.childNodes[i].classList && node.childNodes[i].classList.contains (className)) {
+               result.push (node.childNodes[i]);
+             }
+             recurse (node.childNodes[i]);
+        
+        }
+     
+  }
+
+ recurse (document.body);
+ return result;
+
 };
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,90 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+	var isArray = Array.isArray(obj);
+	var isObject = false;
+
+	if (Object.prototype.toString.call(obj) === "[object Object]") {
+		isObject = true;
+	}
+
+	var recurse = function(obj, isObjectAnArray, isObjectAnObject) {
+		if (typeof obj === "function") {
+			if (isObjectAnArray) {
+				return null;
+			} else {
+				return undefined;
+			}
+		} else if (typeof obj === "number") {
+			if (isObjectAnArray || isObjectAnObject) {
+				return obj;
+			} else {
+				return "" + obj;
+			}
+
+		} else if (typeof obj === "string") {
+          if (isObjectAnArray) {
+             return '"'+obj+'"';
+          } else {
+            return '"'+obj+'"';
+            
+          }
+			
+
+		} else if (typeof obj === "undefined") {
+			if (isObjectAnArray) {
+				return null;
+			} else {
+				return undefined;
+			}
+
+		} else if (typeof obj === "boolean") {
+
+			if (isObjectAnArray || isObjectAnObject) {
+				return obj;
+			} else {
+				return "" + obj;
+			}
+
+		} else if (obj === null ) {
+          
+          return "" + null;
+                
+        }else if (Array.isArray(obj)) {
+
+			return "[" +
+				obj.map ( function(value) {
+					return recurse(value, isObjectAnArray, isObjectAnObject);
+
+				}).join(",") + "]";
+
+
+		} else if (Object.prototype.toString.call(obj) === "[object Object]") {
+
+			var result = [];
+
+			for (var key in obj) {
+				if (typeof obj[key] === "function" || typeof obj[key] === undefined) {
+					break;
+
+				}
+				var val = recurse(obj[key], isObjectAnArray, isObjectAnObject);
+				if (val !== null) {
+                  var stringOne =  '"'+ key +'"';
+                 
+					result.push(stringOne  +  ":" +val);
+				}
+
+			}
+
+			return "{" + result.join(",") + "}";
+		}
+	};
+
+	var answer = recurse(obj, isArray, isObject);
+	return answer;
+
 };
+
+
+
